2782517aaa41b27c2e72c5e4986ceb3a94de89e5
Lecnier Freeman <lecnier.freeman@tklabs.com>
2021-08-13 16:39:38 -0400

    Update googlemaps to 5.1.0 and remove GooglePlaces

--- Package.swift
+++ Package.swift
@@ -23,46 +23,23 @@ let package = Package(
                 "GoogleMapsBase",
                 "GoogleMapsCore"
             ]
-        ),
-        .library(
-            name: "GoogleMapsM4B",
-            targets: [
-                "GoogleMapsM4B"
-            ]
-        ),
-        .library(
-            name: "GooglePlaces",
-            targets: [
-                "GooglePlaces",
-                "GoogleMapsBase"
-            ]
         )
     ],
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/5.0.0/GoogleMaps.xcframework.zip",
-            checksum: "a91877f99f5f588731c5b1662d3fff1471d5139e3c048763d901f4e2a4e84d5d"
+            url: "https://github.com/lecniers/GoogleMaps-SP/releases/download/5.1.0/GoogleMaps.xcframework.zip",
+            checksum: "0e46a4fce53bd73262073c822f0e40db4700eba5e33c73650baa35959d0469d7"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/5.0.0/GoogleMapsBase.xcframework.zip",
-            checksum: "bfffbfc750caae8cff22ed489720eff9d1efa58ebe883c058f6a6ce52d970205"
+            url: "https://github.com/lecniers/GoogleMaps-SP/releases/download/5.1.0/GoogleMapsBase.xcframework.zip",
+            checksum: "d46c93a7e079b25be9837e0d78502cd68d366c3f9487bcff7df00e74ccc0f490"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/5.0.0/GoogleMapsCore.xcframework.zip",
-            checksum: "192894bdaf54965bad8541784d4fff261779d1ad268d9df6ad28c3ad06fe7047"
-        ),
-        .binaryTarget(
-            name: "GoogleMapsM4B",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/5.0.0/GoogleMapsM4B.xcframework.zip",
-            checksum: "d63ff2a74acbc71a37f7a615de11ab47b6fe9c462b2e68feff764a7153e1a99c"
-        ),
-        .binaryTarget(
-            name: "GooglePlaces",
-            url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/5.0.0/GooglePlaces.xcframework.zip",
-            checksum: "703694603a35710277f98425d7f7b24ba409524dfd7275fcce442478980a8bee"
+            url: "https://github.com/lecniers/GoogleMaps-SP/releases/download/5.1.0/GoogleMapsCore.xcframework.zip",
+            checksum: "dd73477e9799cda685c195eb7f7f000cf914f7810b6d440614fcc18ffd383cd4"
         )
     ]
 )
